f3b9d8339f1bae0b77ba0856ea87ae5d525832ef
NavasMuhammed <navas3444a3@gmail.com>
2022-12-19 21:51:21 +0530

    feat: left-menu-items

--- src/components/LeftMenu/leftMenu.jsx
+++ src/components/LeftMenu/leftMenu.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LeftMenuItems } from './data'
 import "./leftMenu.scss"
 function LeftMenu() {
   return (
@@ -10,18 +11,15 @@ function LeftMenu() {
         <div className="circle-3"></div>
       </div>
       <div className="left-menu-items-container">
-        <div className="left-menu-items">
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAhUlEQVRIid2UUQrAIAxDy07n/Y+gB3n7cw7UNR2IW36TEkxszX4DIAGFPjKQItrWIA8G6mBE2xoAMHjdjVO0hzfCKLY1KGZXHC1a/gaxgzQpeviL3AYKlnbgzpXgorlzneiq/tWzlb7WdRDKVYGSqxLRM9Hh9uxghYFywELHTlk0/dh9Fic8pXankphXVAAAAABJRU5ErkJggg=="/>
-          <span>Dashboard</span>
-        </div>
-        <div className="left-menu-items">
-          <img src="" alt="" />
-          <span>hi</span>
-        </div>
-        <div className="left-menu-items">
-          <img src="" alt="" />
-          <span>hi</span>
-        </div>
+        {LeftMenuItems.map((item, index) => {
+          return (
+            <div key={index} className="left-menu-items">
+              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAhUlEQVRIid2UUQrAIAxDy07n/Y+gB3n7cw7UNR2IW36TEkxszX4DIAGFPjKQItrWIA8G6mBE2xoAMHjdjVO0hzfCKLY1KGZXHC1a/gaxgzQpeviL3AYKlnbgzpXgorlzneiq/tWzlb7WdRDKVYGSqxLRM9Hh9uxghYFywELHTlk0/dh9Fic8pXankphXVAAAAABJRU5ErkJggg==" />
+              <span>{item.title}</span>
+            </div>
+          )
+        })
+        }
       </div>
 
     </div>

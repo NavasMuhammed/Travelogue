bc364ba8fc96cb08cad3813e8e8b4eaf68228286
NavasMuhammed <navas3444a3@gmail.com>
2023-01-12 01:11:42 +0530

    feat: bottom-card

---
+++ src/components/widgets/cardSecond/data.js
@@ -0,0 +1,17 @@
+export const cardData = [
+    {
+        image: "/images/Phi Phi islands.png",
+        title: "Trip to italy",
+        time: "18-10-June",
+    },
+    {
+        image: "/images/kudahuado.png",
+        title: "Trip to Rome",
+        time: "20-8-April",
+    },
+    {
+        image: "/images/greece.png",
+        title: "Trip to india",
+        time: "11-05-May",
+    },
+]
